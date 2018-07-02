b0f85692ac9047ea21e6414fd52f6f6ad8d99680
Maricela Sanchez <mayela0x14@gmail.com>
2018-07-02 10:44:05 -0500

    Create components gallery, about, contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
